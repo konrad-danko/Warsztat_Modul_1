04de24890a713e873a3ec75d39d40903640dfb51
Konrad Dańko <konrad.danko@gmail.com>
2020-01-14 00:04:02 +0100

    Rozwiązanie Zadania 5 z Warsztatu 1

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+_
+/src/com/company/Zad1_Gra_zgad_liczb1/Main1.java,b/7/b778e2dca04e65e659e9a6c238428d474bc47a46
+U
+%src/com/company/Zad2_Lotto/Main1.java,5/8/58786cf64a1d86908a58f33e225d76ae2a50ca9b
+_
+/src/com/company/Zad3_Gra_zgad_liczb2/Main1.java,1/e/1e6284cd2449511ce6073a12db52a721d04b1d8e
+V
+&src/com/company/Zad4_Kostka/Main1.java,c/8/c8c3f2947a179b2ee0b28bffff7d2fa6d21ccd5b
+A
+popular_words.txt,8/b/8b0ae5901955efac32736f9ed6a09eff9091310d
+J
+filtered_popular_words.txt,5/2/523b50a9724fff578a3c135d5971a3ef7432db5b
+\
+,src/com/company/Zad5_Wyszukiwarka/Main1.java,6/1/61c085b661f1d9bac729503c9314785045ea51c6
--- src/com/company/Zad5_Wyszukiwarka/Main1.java
+++ src/com/company/Zad5_Wyszukiwarka/Main1.java
@@ -11,5 +11,73 @@ wykluczone.*/
 
 package com.company.Zad5_Wyszukiwarka;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main1 {
+    public static void main(String[] args) throws IOException {
+
+        //pobieranie danych z Internetu i zapis do pliku:
+        //deklaracja obiektu klasy Connection (ściąganie danych z Internetu)
+        Connection connect = Jsoup.connect("https://www.onet.pl/");
+        FileWriter filWrit = new FileWriter("popular_words.txt", false);
+        //deklaracja tablicy ze znakami do usunięcia, na wszelki wypadek są wyescapowane
+        //ale nie wiem jak tu dodać znak cudzysłowu: ",
+        String[] charsRemoved = {"\\–","\\]","\\[","\\«","\\»","\\.","\\-","\\?","\\!","\\:","\\,","\\'","\\+"};
+        try {
+            Document document = connect.get(); //ściąganie danych z Internetu
+            Elements links = document.select("span.title"); //ściąganie danych z Internetu
+            for (Element elem : links) {
+                String line = elem.text(); //zamiana na String obiektu klasy Element
+                System.out.println(line);
+                for (String s: charsRemoved) {
+                    line = line.replaceAll(s , "");
+                }
+                String[] words = line.split(" ");
+                for (String s: words) {
+                    if (s.length()>3) {
+                        filWrit.append(s).append("\n");
+                    }
+                }
+            }
+            filWrit.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //tablica słów wykluczonych
+        String[] excluded = {"oraz","ponieważ","albo","tylko"};
+
+        //Zapis do nowego pliku z wykluczeniem słów z tabeli "excluded"
+        File filRead = new File("popular_words.txt");
+        FileWriter filWritFilt = new FileWriter ("filtered_popular_words.txt",false);
+        Scanner scan = new Scanner(filRead);
+        int goFlag;
+        try {
+            while (scan.hasNextLine()) {
+                String lineRead = scan.nextLine();
+                goFlag=0;
+                for (String s : excluded) {
+                    if (s.equalsIgnoreCase(lineRead)) {
+                        goFlag = 1;
+                        break;
+                    }
+                }
+                if (goFlag==0) {
+                    filWritFilt.append(lineRead).append("\n");
+                }
+            }
+            filWritFilt.close();
+        } catch (FileNotFoundException ex1) {
+            System.out.println(ex1);
+        }
+    }
 }
